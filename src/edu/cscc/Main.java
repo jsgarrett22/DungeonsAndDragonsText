e4229807f824a470d0cb9d0966daf77f8ff22ee4
Joshua S. Garrett <73357958+jsgarrett22@users.noreply.github.com>
2020-12-28 19:02:32 -0500

    Delete Main.java

--- src/edu/cscc/Main.java
+++
@@ -1,8 +0,0 @@
-package edu.cscc.main;
-
-public class Main {
-    public static void main(String[] args) {
-        Player player1 = new Player("Joshua", "Human", "Warrior", 10);
-        System.out.println("Player 1 Stats: " + "\n" + player1.toString());
-    }
-}
