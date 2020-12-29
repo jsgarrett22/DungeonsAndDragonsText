5a7a1b7e2400d685ebe7296c533d2b5e68bc653c
Joshua S. Garrett <73357958+jsgarrett22@users.noreply.github.com>
2020-12-28 19:03:05 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-package com.frontier.entities;
-
-public class Main {
-    public static void main(String[] args) {
-        Player player1 = new Player("Joshua", "Human", "Warrior", 10);
-        System.out.println("Player 1 Stats: " + "\n" + player1.toString());
-    }
-}
